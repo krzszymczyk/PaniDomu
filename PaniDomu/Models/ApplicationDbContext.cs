759e8dbb03cdf9ece5e07b4129d23a18b406f213
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2017-09-30 16:01:24 +0200

    Models created

--- PaniDomu/Models/ApplicationDbContext.cs
+++ PaniDomu/Models/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace PaniDomu.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ PaniDomu/Models/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaniDomu.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
